c5713c2a7f2030ed8e1c8485c092f89ed6de4079
christian <cxs1529@miami.edu>
2025-08-14 20:15:46 -0400

    html example for sg668 and units added to table headers

--- app.py
+++ app.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 #sgid = "668"
 #dbname = sgid + ".db"
 #logtable = "log_table"
+sgid_list = ["609", "668"]
 
 @app.route("/")
 @app.route("/<sgid>")
 def table(sgid):
-
-    dbname = sgid + ".db"
-    logtable = "log_table"
-    df = files.read_database(dbname, logtable, "descending")
+    if sgid in sgid_list:
+        dbname = sgid + ".db"
+        logtable = "log_table"
+        df = files.read_database(dbname, logtable, "descending")
     
-    table_data = df_html.convert_to_table(df) # use |safe keyword in html template so jinja doesn't change characters in strings
+        table_data = df_html.convert_to_table(df) # use |safe keyword in html template so jinja doesn't change characters in strings
+    else:
+        table_data = "<div> Glider ID not processed </div>"
 
     return render_template("table.html", table_data=table_data, sgid=sgid)
 
--- ncmodules/df_html.py
+++ ncmodules/df_html.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-# define warning threshold values
+# define warning threshold values for conditional formatting
 int_Humidity_thd = {"min":40.0, "max": 70.0}
-int_Pressure_thd = {"min":9.4, "max": 10.0}
+int_Pressure_thd = {"min":9.2, "max": 10.0}
 int_Temperature_thd = {"min":15, "max": 35}
 SM_angle_thd = {"min":-75, "max": -50}
 SM_depth_thd = {"min":0.5, "max": 1.5}
@@ -10,9 +10,10 @@ CALLS_thd = {"min":0, "max": 5}
 dac_velocity_thd = {"min":0, "max": 1}
 surf_velocity_thd = {"min":0, "max": 71} 
 
-# return a formated html table from a dataframe
+
+# returns a formated html table based on a dataframe
 def convert_to_table(df):
-    
+    # headers to display in table
     filter_headers = ['dive', 'time_start', 'time_end', 'gps_lat_start', 'gps_lon_start',
        'gps_lat_end', 'gps_lon_end', 'D_TGT', 'D_ABORT', 'T_DIVE', 'T_MISSION',
        'T_ABORT', 'C_PITCH', 'PITCH_GAIN', 'C_ROLL_DIVE', 'C_ROLL_CLIMB',
@@ -22,38 +23,28 @@ def convert_to_table(df):
        'dac_heading', 'surf_velocity', 'surf_heading', 'glider_sog',
        'glider_dog', 'glider_hdg', 'glider_dive_time' ] # 'filePath' removed
     
-    #print(df.keys())  
+    #print(df.keys())  # prints all dataframe keys or headers
 
+    # build header html string
     print(">html for header:")  
     html_table_headers = "<tr class=\"table_header\">\n"
     for j in df.keys():
         if j in filter_headers:
-            html_table_headers = html_table_headers + f"\n\t<th class=\"table_header\"> {j} </th>" 
+            units = get_header_units(j)
+            html_table_headers = html_table_headers + f"\n\t<th class=\"table_header\"> {j} {units} </th>" 
 
     html_table_headers = html_table_headers + "\n</tr>\n"
     print(html_table_headers)
 
-
-    print(">html for data:")  
-    html_table_data = ""
     # create html table string assuming diveN is in descending order
+    print(">html for data:")  
+    html_table_data = ""    
     for i in range(0, len(df)):
         # start table row
         html_table_data = html_table_data + "\t\n<tr class=\"table_row\">\n"
         # iterate over data fields
-        for col in filter_headers:
-            
+        for col in filter_headers:            
             data_value, data_class = get_data_class(i, col, df)
-
-            # # check class conditions for particular parameters
-            # dataclass = "data_normal"
-            # if col == "int_Humidity" and df.loc[i,col] > 65.0:
-            #     dataclass = "data_warning"
-            # if col == "C_ROLL_DIVE":
-            #     if i+1 < len(df):
-            #         if df.loc[i,col] != df.loc[i+1,col]:
-            #             dataclass = "data_change"
-
             # build each table data entry
             html_table_data = html_table_data + f"\t<td class={data_class}> {data_value} </td>\n"
         # end table row
@@ -63,22 +54,28 @@ def convert_to_table(df):
     return html_table_headers + html_table_data
 
 
+# returns the value and style class for a given dataframe item
 def get_data_class(i, col, df):
+    # default values to return
     data_class = "data_normal"
     data_value = df.loc[i,col]
-
+    # parameter classification for styling
     ctrl_parameters_int = ['D_TGT', 'D_ABORT', 'T_DIVE', 'T_MISSION',
        'T_ABORT', 'C_PITCH', 'C_ROLL_DIVE', 'C_ROLL_CLIMB',
-       'C_VBD', 'MAX_BUOY', 'NAV_MODE', 'HEADING']
+       'C_VBD', 'MAX_BUOY', 'NAV_MODE', 'HEADING', 'SM_CC']
     ctrl_parameters_float = ['PITCH_GAIN']
     ctrl_parameters_str = ['TGT_name']
+    gps_locations = ['gps_lat_start', 'gps_lon_start', 'gps_lat_end', 'gps_lon_end']
+    velocities = ['dac_velocity',  'surf_velocity', 'glider_dog' ]
+    headings = ['dac_heading', 'surf_heading', 'glider_hdg']
 
+    # set style class based on parameter value exceeding normal values
     if col == "int_Humidity":
-        data_value = round(data_value, 2)
+        data_value = round(data_value, 1)
         if data_value <= int_Humidity_thd["min"] or data_value >= int_Humidity_thd["max"]:
             data_class = "data_warning"
     elif col == "int_Pressure":
-        data_value = round(data_value, 2)
+        data_value = round(data_value, 1)
         if data_value <= int_Pressure_thd["min"] or data_value >= int_Pressure_thd["max"]:
             data_class = "data_warning"
     elif col == "int_Temperature":
@@ -86,15 +83,15 @@ def get_data_class(i, col, df):
         if data_value <= int_Temperature_thd["min"] or data_value >= int_Temperature_thd["max"]:
             data_class = "data_warning"        
     elif col == "SM_angle":
-        data_value = round(data_value, 2)
+        data_value = int(round(data_value, 0))
         if data_value <= SM_angle_thd["min"] or data_value >= SM_angle_thd["max"]:
             data_class = "data_warning"        
     elif col == "SM_depth":
-        data_value = round(data_value, 2)
+        data_value = round(data_value, 1)
         if data_value <= SM_depth_thd["min"] or data_value >= SM_depth_thd["max"]:
             data_class = "data_warning"         
     elif col == "CALLS":
-        data_value = round(data_value, 0)
+        data_value = int(data_value)
         if data_value <= CALLS_thd["min"] or data_value >= CALLS_thd["max"]:
             data_class = "data_warning"        
     elif col == "dac_velocity":
@@ -104,7 +101,9 @@ def get_data_class(i, col, df):
     elif col == "surf_velocity":
         data_value = round(data_value, 2)
         if data_value <= surf_velocity_thd["min"] or data_value >= surf_velocity_thd["max"]:
-            data_class = "data_warning"         
+            data_class = "data_warning"  
+    elif col == "glider_sog":
+            data_value = str(round(data_value,2)) + " | " + str(round(data_value * 3600 /1000, 2))
     elif col in ctrl_parameters_int or col in ctrl_parameters_float or col in ctrl_parameters_str:
         if i+1 < len(df):            
             if df.loc[i,col] != df.loc[i+1,col]:
@@ -115,6 +114,46 @@ def get_data_class(i, col, df):
             data_value = round(data_value, 3)
         else:
             pass
+    elif col in gps_locations:
+        data_value = f"{round(data_value, 3):.3f}"
+    elif col in velocities:
+        data_value = f"{round(data_value, 2):.2f}"
+    elif col in headings:
+        data_value = int(round(data_value, 0))
+    elif col == "glider_dive_time":
+        data_value = str(int(data_value)) + " | " + str(int(data_value/60.0)) + " | " + str(round(data_value/60.0/60.0, 1))
+    else:
+        pass
+    
 
     return data_value, data_class
 
+def get_header_units(j):
+    units = ""
+
+    if j in ['gps_lat_start', 'gps_lon_start', 'gps_lat_end', 'gps_lon_end']:
+        units = "[dd.ddd]"
+    elif j == "int_Humidity":
+        units = "[%]"
+    elif j == "int_Pressure":
+        units = "[psi]" 
+    elif j == "int_Temperature":
+        units = "[" + chr(176) + "C]"
+    elif j == "SM_angle":
+       units = "[" + chr(176) + "]"
+    elif j == "SM_depth":
+       units = "[m]"
+    elif j in ['dac_velocity', 'surf_velocity' ]:
+       units = "[m/s]"
+    elif j in ['dac_heading', 'surf_heading', 'glider_hdg']:
+       units = "[" + chr(176) + "]"
+    elif j == "glider_dive_time":
+       units = "[s][min][h]"
+    elif j == "glider_dog":
+       units = "[km]"
+    elif j == "glider_sog":
+        units = "[m/s][km/h]"
+    else:
+        pass
+    
+    return units
